a5e6e7d4707faa12569bbfebb02f1572bc729b46
Juan Pablo Romero Bermudez <jprb@kth.se>
2021-12-01 18:26:47 +0100

    Sync through arrays

--- one_cam_sync.py
+++ one_cam_sync.py
@@ -8,7 +8,6 @@ import cv2
 import sys
 
 
-
 class GracefulKiller:
   kill_now = False
   def __init__(self):
@@ -21,11 +20,11 @@ class GracefulKiller:
 
 # python3 one_cam_visuals.py <cam_id>
 
-def get_events(cam_id, cam_shape, arr):
+def get_events(cam_id, cam_shape, arr, ready):
 
 
   delta_t = 0.001 #1ms
-  fps = 20
+  fps = 50
   max_sfc = 1/delta_t/fps # 40 for 25fps and 1000 sb/sec
   
   port_nb = 7770 + cam_id
@@ -40,55 +39,59 @@ def get_events(cam_id, cam_shape, arr):
 
   mat = np.zeros(cam_shape)
   with NetworkEventInput(address='172.16.222.46', port=port_nb) as i:
-      for event in i:
+    for event in i:
 
-          if killer.kill_now:
-              break
+      if killer.kill_now:
+          break
 
-          t1 = time.time()
-          
-          mat[event.y, event.x] += 1
+      t1 = time.time()
+      
+      mat[event.y, event.x] += 1
+      arr[event.y*640 + event.x] += 1
 
-          ev_count += 1
+      ev_count += 1
 
 
-          t_current = time.time() 
+      t_current = time.time() 
 
-          # Check if new subfram needs to be started
-          if t_current - t_sfc >= (delta_t):
-              sfc += 1
+      # Check if new subfram needs to be started
+      if t_current - t_sfc >= (delta_t):
+        sfc += 1
 
-              # Check if visual frame needs to be shown
-              if sfc >= max_sfc:
-                
-                arr[:] = mat.reshape((np.prod(cam_shape),1))
-                # tam = np.array(arr).reshape((480,640))
-                # cv2.imshow("frame", mat)
-                # cv2.waitKey(1)  
-                sfc = 0      
+        # Check if visual frame needs to be shown
+        if sfc >= max_sfc:
+          ready.value = 1
 
-                mat = mat*0.1
+          sfc = 0      
 
-              t_sfc = t_current
+          mat = mat*0.1
+          arr[:] = np.zeros(np.prod(cam_shape))
 
-              if t_current - t_evc >= 1:
-                print("%d events per sec" %(ev_count))
-                ev_count = 0
-                t_evc = t_current
-              # print("Cam %d @ %ld : (%d,%d) [%d]" %(cam_id, event.timestamp, event.x, event.y, event.polarity))
+        t_sfc = t_current
+
+        if t_current - t_evc >= 1:
+          print("%d events per sec" %(ev_count))
+          ev_count = 0
+          t_evc = t_current
+        # print("Cam %d @ %ld : (%d,%d) [%d]" %(cam_id, event.timestamp, event.x, event.y, event.polarity))
           
         
 
-def visualize(cam_id, cam_shape, arr):
+def visualize(cam_id, cam_shape, tam, ready):
 
   while not killer.kill_now:
-    tam = np.array(arr).reshape(cam_shape)
-    cv2.imshow("frame", tam)
-    cv2.waitKey(1) 
+    # tam = np.array(arr).reshape(cam_shape)
+    if ready.value == 1:
+      cv2.imshow("frame", tam)
+      cv2.waitKey(1) 
+      ready.value = 0
+    
 
 if __name__ == '__main__':
     
   global killer
+
+  ready = False
 
   print(mp.cpu_count())
 
@@ -96,10 +99,12 @@ if __name__ == '__main__':
 
   cam_shape = (480,640)
 
-  arr = mp.Array('d', np.zeros((np.prod(cam_shape),1)))
+  ready = mp.Value('i', 0)
+  arr = mp.Array('d', np.zeros((np.prod(cam_shape),1)), lock=mp.Lock())
+  tam = np.frombuffer(arr.get_obj(), dtype="d").reshape(cam_shape)
 
-  p1 = mp.Process(target=get_events, args=(1, cam_shape, arr,))
-  v1 = mp.Process(target=visualize, args=(1, cam_shape, arr,))
+  p1 = mp.Process(target=get_events, args=(1, cam_shape, arr, ready))
+  v1 = mp.Process(target=visualize, args=(1, cam_shape, tam, ready))
 
 
 
