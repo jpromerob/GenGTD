57b4623029f16aea5dc9a5d1f5c795ebaa7a7962
Juan Pablo Romero Bermudez <jprb@kth.se>
2021-12-01 19:03:51 +0100

    Factorization (when creating processes)

--- three_cam_sync.py
+++ three_cam_sync.py
@@ -37,19 +37,18 @@ def get_events(cam_id, cam_shape, arr, rdy):
   ev_count = 0
   sfc = 0 # sub-frame counter
 
-  mat = np.zeros(cam_shape)
   with NetworkEventInput(address='172.16.222.46', port=port_nb) as i:
     for event in i:
 
       if killer.kill_now:
           break
 
+      ev_count += 1
+      
       t1 = time.time()
       
-      mat[event.y, event.x] += 1
       arr[event.y*640 + event.x] += 1
 
-      ev_count += 1
 
 
       t_current = time.time() 
@@ -64,25 +63,23 @@ def get_events(cam_id, cam_shape, arr, rdy):
 
           sfc = 0      
 
-          mat = mat*0.1
           arr[:] = np.zeros(np.prod(cam_shape))
 
         t_sfc = t_current
 
         if t_current - t_evc >= 1:
-          print("%d events per sec" %(ev_count))
+          # print("%d events per sec" %(ev_count))
           ev_count = 0
           t_evc = t_current
         # print("Cam %d @ %ld : (%d,%d) [%d]" %(cam_id, event.timestamp, event.x, event.y, event.polarity))
           
         
 
-def visualize(cam_id, cam_shape, tam, rdy):
+def visualize(cam_id, img, rdy):
 
   while not killer.kill_now:
-    # tam = np.array(arr).reshape(cam_shape)
     if rdy.value == 1:
-      cv2.imshow("frame", tam)
+      cv2.imshow("Camera #" + str(cam_id), img)
       cv2.waitKey(1) 
       rdy.value = 0
     
@@ -91,7 +88,6 @@ if __name__ == '__main__':
     
   global killer
 
-  rdy = False
 
   print(mp.cpu_count())
 
@@ -99,42 +95,30 @@ if __name__ == '__main__':
 
   cam_shape = (480,640)
 
-  rdy_1 = mp.Value('i', 0)
-  arr_1 = mp.Array('d', np.zeros((np.prod(cam_shape),1)), lock=mp.Lock())
-  tam_1 = np.frombuffer(arr_1.get_obj(), dtype="d").reshape(cam_shape)
-  p1 = mp.Process(target=get_events, args=(1, cam_shape, arr_1, rdy_1))
-  v1 = mp.Process(target=visualize, args=(1, cam_shape, tam_1, rdy_1))
+  rdy = []
+  arr = []
+  img = []
+  pro = []
+  vis = []
 
-  rdy_2 = mp.Value('i', 0)
-  arr_2 = mp.Array('d', np.zeros((np.prod(cam_shape),1)), lock=mp.Lock())
-  tam_2 = np.frombuffer(arr_2.get_obj(), dtype="d").reshape(cam_shape)
-  p2 = mp.Process(target=get_events, args=(2, cam_shape, arr_2, rdy_2))
-  v2 = mp.Process(target=visualize, args=(2, cam_shape, tam_2, rdy_2))
-
-  rdy_3 = mp.Value('i', 0)
-  arr_3 = mp.Array('d', np.zeros((np.prod(cam_shape),1)), lock=mp.Lock())
-  tam_3 = np.frombuffer(arr_3.get_obj(), dtype="d").reshape(cam_shape)
-  p3 = mp.Process(target=get_events, args=(3, cam_shape, arr_3, rdy_3))
-  v3 = mp.Process(target=visualize, args=(3, cam_shape, tam_3, rdy_3))
+  for i in range(3):
+    cam_id = i+1
+    rdy.append(mp.Value('i', 0))
+    arr.append(mp.Array('d', np.zeros((np.prod(cam_shape),1)), lock=mp.Lock()))
+    img.append(np.frombuffer(arr[i].get_obj(), dtype="d").reshape(cam_shape))
+    pro.append(mp.Process(target=get_events, args=(cam_id, cam_shape, arr[i], rdy[i])))
+    vis.append(mp.Process(target=visualize, args=(cam_id, img[i], rdy[i])))
 
 
-
-
-  p1.start()
-  v1.start()
-  p2.start()
-  v2.start()
-  p3.start()
-  v3.start()
+  for i in range(3):
+    pro[i].start()
+    vis[i].start()
 
     
 
-  p1.join()
-  v1.join()
-  p2.join()
-  v2.join()
-  p3.join()
-  v1.join()
+  for i in range(3):
+    pro[i].join()
+    vis[i].join()
 
   cv2.destroyAllWindows()
 
