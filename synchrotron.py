b9db8ec6ff3368b40213ea4ca459ac389e7987c3
Juan Pablo Romero Bermudez <jprb@kth.se>
2021-12-02 16:41:46 +0100

    Adding cam poses

--- synchrotron.py
+++ synchrotron.py
@@ -9,6 +9,93 @@ import h5py
 from numpy import genfromtxt
 
 
+def set_cam_poses():
+    cam_poses = np.zeros((3,6))
+
+    # Cam 1
+    cam_poses[0,0] = -0.099 # cam1:cx
+    cam_poses[0,1] = 0.968 # cam1:cy
+    cam_poses[0,2] = 1.363 # cam1:cz
+    cam_poses[0,3] = (math.pi/180)*(-71.499) # cam1:alpha
+    cam_poses[0,4] = (math.pi/180)*(16.753) # cam1:beta
+    cam_poses[0,5] = (math.pi/180)*(-20.992) # cam1:gamma
+
+    # Cam 2
+    cam_poses[1,0] = -0.570 # cam2:cx
+    cam_poses[1,1] = 0.970 # cam2:cy
+    cam_poses[1,2] = 1.395 # cam2:cz
+    cam_poses[1,3] = (math.pi/180)*(-62.113) # cam2:alpha
+    cam_poses[1,4] = (math.pi/180)*(-42.374) # cam2:beta
+    cam_poses[1,5] = (math.pi/180)*(-6.134) # cam2:gamma
+
+    # Cam 3
+    cam_poses[2,0] = -0.664 # cam3:cx
+    cam_poses[2,1] =  0.979 # cam3:cy
+    cam_poses[2,2] =  0.538 # cam3:cz
+    cam_poses[2,3] = (math.pi/180)*(148.698)# cam3:alpha
+    cam_poses[2,4] = (math.pi/180)*(-46.056)# cam3:beta
+    cam_poses[2,5] = (math.pi/180)*(148.752)# cam3:gamma
+
+    return cam_poses
+
+
+'''
+This function returns the matrix 'c2w' that converts coordinates in camera space to coordinates in world space.
+'''
+def get_transmats(cam_poses):
+    
+    c2w = np.zeros((4,4,3))
+    for i in range(3): # Cam 1, 2, 3
+        
+        cx = cam_poses[i,0]
+        cy = cam_poses[i,1]
+        cz = cam_poses[i,2]
+        alpha = cam_poses[i,3]
+        beta = cam_poses[i,4] 
+        gamma = cam_poses[i,5]
+
+        # Transformation matrices (translation + rotations around x, y, z)
+        mat_tran = np.array([[1,0,0,cx],
+                             [0,1,0,cy],
+                             [0,0,1,cz],
+                             [0,0,0,1]])
+
+        mat_rotx = np.array([[1,0,0,0],
+                             [0,math.cos(alpha), -math.sin(alpha),0],
+                             [0, math.sin(alpha), math.cos(alpha),0],
+                             [0,0,0,1]])
+
+        mat_roty = np.array([[math.cos(beta), 0, math.sin(beta),0],
+                             [0,1,0,0],
+                             [-math.sin(beta), 0, math.cos(beta),0],
+                             [0,0,0,1]])
+
+
+        mat_rotz = np.array([[math.cos(gamma), -math.sin(gamma), 0, 0],
+                             [math.sin(gamma), math.cos(gamma),0, 0],
+                             [0,0,1,0],
+                             [0,0,0,1]])
+
+        # General transformation matrix 'camera to world' (c2w)
+        c2w[:,:,i] = mat_tran.dot(mat_rotx).dot(mat_roty).dot(mat_rotz)
+    
+    
+    return c2w
+
+'''
+This function defines object pose from camera perspective
+'''
+def define_object_pose(c2w, ground_truth):
+    
+    perspective = np.zeros((4,3)) # coordinates|cameras
+    # Checking output of each camera
+    for i in range(3): # Cam 1, 2, 3
+        w2c = np.linalg.inv(c2w[:,:,i])
+        perspective[:, i] = w2c.dot(ground_truth)
+
+    return perspective[0:3,:]
+
+
 if __name__ == "__main__":
 
 
